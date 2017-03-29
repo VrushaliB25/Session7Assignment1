eb3dbbbd15560c8a494ae16db2b5fb1161867da8
VrushaliB25 <vrbhoite@gmail.com>
2017-03-29 20:23:13 +0530

    Seession7Assignment1

---
+++ Session7Assignment1/src/session7Assign1.java
@@ -0,0 +1,14 @@
+
+public class session7Assign1 {
+	public static void main(String s[])
+	{
+		//To write a program to convert an int variable to String using an inbuilt function of String class.
+		int num1=50;
+		String str;
+		System.out.println("The number is  "+num1);
+		str=String.valueOf(num1);
+			System.out.println("The number "+num1+" is converted into string  "+str);
+	}
+	
+}
+
